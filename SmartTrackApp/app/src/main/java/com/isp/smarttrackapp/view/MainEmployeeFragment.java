338cb45b7d17182bef873ac530172971f53978be
federico_cabrera <fedegcab@gmail.com>
2020-06-11 01:05:28 -0300

    Interacción con fragment con mapa

--- SmartTrackApp/app/src/main/java/com/isp/smarttrackapp/view/MainEmployeeFragment.java
+++ SmartTrackApp/app/src/main/java/com/isp/smarttrackapp/view/MainEmployeeFragment.java
@@ -6,32 +6,27 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 import com.isp.smarttrackapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainEmployeeFragment extends Fragment implements OnMapReadyCallback {
+public class MainEmployeeFragment extends Fragment {
 
     private Context thisContext;
     private View mainView;
-    private GoogleMap googleMap;
-    private MapView mapView;
+    private NavController navController;
+    private Button btnNewTraject;
 
     public MainEmployeeFragment() {
         // Required empty public constructor
@@ -44,6 +39,22 @@ public class MainEmployeeFragment extends Fragment implements OnMapReadyCallback
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        btnNewTraject = view.findViewById(R.id.em_btn_new_traject);
+
+        btnNewTraject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_mainEmployeeFragment_to_employeeMapFragment);
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -52,32 +63,8 @@ public class MainEmployeeFragment extends Fragment implements OnMapReadyCallback
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mapView = (MapView) mainView.findViewById(R.id.emain_map);
-        if(mapView != null){
-            mapView.onCreate(null);
-            mapView.onResume();
-            mapView.getMapAsync(this);
-        }
-    }
-
-    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-
-        LatLng place = new LatLng(-34.8856045, -56.1875722);
-
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
-
-        this.googleMap.addMarker(new MarkerOptions().position(place).title("Empleado"));
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(place));
-        this.googleMap.animateCamera(zoom);
-    }
 }
